bdfb58f94db214d33dd9ba9415ff6d7c945498af
clusterDuck.js <sreeadarsh4@gmail.com>
2026-03-17 15:35:35 +0530

    feat: initialize project with React, Vite, and ESLint setup
    - Added package.json with dependencies for React and Vite
    - Created main application structure with App component
    - Implemented Home and Hero components
    - Added Footer and Header components (files created)
    - Created About page (files created)
    - Added CSS styles for components and global styles
    - Included SVG assets for React and Vite logos
    - Configured Vite for React application

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './hero.css'
+function Hero() {
+  return (
+    <div>Hero</div>
+  )
+}
+
+export default Hero
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './home.css'
+import Hero from '../../Components/Hero/Hero'
+
+function Home() {
+  return (
+    <div>
+        <Hero />
+    </div>
+  )
+}
+
+export default Home
