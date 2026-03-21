0b05941da1c645945071d4254ab002b809b535fb
clusterDuck.js <sreeadarsh4@gmail.com>
2026-03-22 01:09:38 +0530

    Remove unused SVG files and update Header component with new logo and styling

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,31 +1,36 @@
 import React from 'react'
 import { LuSearch, LuShoppingBag, LuUser } from 'react-icons/lu'
 import { NavLink } from 'react-router-dom'
+import LOGO from '../../assets/puchi_logo_tran.svg'
+import './header.css'
 
 function Header() {
   return (
-    <header>
-        <nav>
-            <NavLink to="/" end className="nav-link">
-            Home
-          </NavLink>
-          <NavLink to="/" end className="nav-link">
-            Shop
-          </NavLink>
-          <NavLink to="/" end className="nav-link">
-            About
-          </NavLink>
-          <NavLink to="/" end className="nav-link">
-            <LuSearch />
-          </NavLink>
-          <NavLink to="/" end className="nav-link">
-            <LuUser />
-          </NavLink>
-          <NavLink to="/" end className="nav-link">
-            <LuShoppingBag />
-          </NavLink>
-          
-        </nav>
+    <header className='flex space-btween align-center'>
+      <img className='logo' src={LOGO} alt="Puchi Puchi Logo" />
+      <nav className="header-links">
+        <NavLink to="/" end className="nav-link">
+          Home
+        </NavLink>
+        <NavLink to="/" end className="nav-link">
+          Shop
+        </NavLink>
+        <NavLink to="/" end className="nav-link">
+          About
+        </NavLink>
+      </nav>
+      <nav className="header-btns">
+        <NavLink to="/" end className="nav-link">
+          <LuSearch />
+        </NavLink>
+        <NavLink to="/" end className="nav-link">
+          <LuUser />
+        </NavLink>
+        <NavLink to="/" end className="nav-link">
+          <LuShoppingBag />
+        </NavLink>
+      </nav>
+
     </header>
   )
 }
